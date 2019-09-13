b5259fcccb93fe32d6d94d5900e783e1429988ad
Yurii <ymr999@gmail.com>
2019-09-13 13:59:53 +0300

    Task #1

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
 'use strict';
 
-var money;
-var time;
+let money = prompt("Your months budget?", ""),
+    time = prompt("PLease enter date in format YYYY-MM-DD", "");
 
-confirm("");
+let question1 = "Your current spends?",
+    question2 = "How much?";    
+
+let appData = {
+    budget: money,
+    timeData: time,
+    expenses: {},
+    optionalExpenses: {},
+    income: [],
+    savings: false
+};
+
+let a1 = prompt(question1, ''),
+    a2 = prompt(question2, ''),
+    a3 = prompt(question1, ''),
+    a4 = prompt(question2, '');
+
+appData.expenses.a1 = a2;
+appData.expenses.a3 = a4;    
+
+alert(appData.budget / 30);
