0104e47278d9b17eebc00e33c0e184f4217c5d84
Yurii <ymr999@gmail.com>
2019-09-17 19:09:43 +0300

    Cycles and conditions added

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 'use strict';
 
-let money = prompt("Ваш бюджет на месяц?", ""),
+let money = +prompt("Ваш бюджет на месяц?", ""),
     time = prompt("Введите дату в формате YYYY-MM-DD", "");
 
 let question1 = "Введите обязательную статью расходов в этом месяце",
@@ -14,13 +14,51 @@ let appData = {
     income: [],
     savings: false
 };
+/* let i=0;
+do {
+    let quest1 = prompt(question1, ''),
+        quest_num2 = prompt(question2, '');
+    i++;
+    if (typeof(quest1) === 'string' && (typeof(quest1) != null) && (typeof(quest_num2)) != null && quest1 != '' 
+    && quest_num2 != '' && quest1.length < 50) {
+        console.log("All questions are done");
+    appData.expenses[quest1] = quest_num2;
+    } 
 
-let a1 = prompt(question1, ''),
-    a2 = prompt(question2, ''),
-    a3 = prompt(question1, ''),
-    a4 = prompt(question2, '');
+} while (i != 2); */
 
-appData.expenses.a1 = a2;
-appData.expenses.a3 = a4;    
+/* let i =0;
+while (i < 2) {
+    let quest1 = prompt(question1, ''),
+        quest_num2 = prompt(question2, '');
+    if (typeof(quest1) === 'string' && (typeof(quest1) != null) && (typeof(quest_num2)) != null && quest1 != '' 
+    && quest_num2 != '' && quest1.length < 50) {
+        console.log("All questions are done");
+    appData.expenses[quest1] = quest_num2;
+    }
+    i++;    
+} */
+ for (let i = 0; i < 2; i++) {
+    let quest1 = prompt(question1, ''),
+        quest_num2 = prompt(question2, '');
+    if (typeof(quest1) === 'string' && (typeof(quest1) != null) && (typeof(quest_num2)) != null && quest1 != '' 
+    && quest_num2 != '' && quest1.length < 50) {
+        console.log("All questions are done");
+    appData.expenses[quest1] = quest_num2;
+    } else {
+        i--;
+    }
+}
 
-alert(appData.budget / 30);
+appData.moneyPerDay =  appData.budget / 30;
+alert("ежедневній бюджет" + appData.moneyPerDay);
+
+if (appData.moneyPerDay < 100) {
+    console.log("Минимальній доход");
+} else if (appData.moneyPerDay > 100){
+    console.log("Средний доход");
+} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000){
+    console.log("хороший достаток");
+} else {
+    console.log("Wrong way situation");
+}
