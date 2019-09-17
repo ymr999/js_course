2c64c6a2e00747b38c87b54ee5a8bb04550ec9ad
Yurii <ymr999@gmail.com>
2019-09-17 19:47:36 +0300

    Small fix

--- script.js
+++ script.js
@@ -27,18 +27,18 @@ do {
 
 } while (i != 2); */
 
-/* let i =0;
-while (i < 2) {
+/*  let i =0;
+while (i != 2) {
     let quest1 = prompt(question1, ''),
         quest_num2 = prompt(question2, '');
     if (typeof(quest1) === 'string' && (typeof(quest1) != null) && (typeof(quest_num2)) != null && quest1 != '' 
     && quest_num2 != '' && quest1.length < 50) {
         console.log("All questions are done");
-    appData.expenses[quest1] = quest_num2;
+        appData.expenses[quest1] = quest_num2;
     }
     i++;    
 } */
- for (let i = 0; i < 2; i++) {
+  for (let i = 0; i < 2; i++) {
     let quest1 = prompt(question1, ''),
         quest_num2 = prompt(question2, '');
     if (typeof(quest1) === 'string' && (typeof(quest1) != null) && (typeof(quest_num2)) != null && quest1 != '' 
@@ -46,12 +46,14 @@ while (i < 2) {
         console.log("All questions are done");
     appData.expenses[quest1] = quest_num2;
     } else {
-        i--;
+        i = i - 1;
+        quest1 = prompt(question1, ''),
+        quest_num2 = prompt(question2, '');
     }
 }
 
 appData.moneyPerDay =  appData.budget / 30;
-alert("ежедневній бюджет" + appData.moneyPerDay);
+alert("Ежедневный бюджет " + appData.moneyPerDay);
 
 if (appData.moneyPerDay < 100) {
     console.log("Минимальній доход");
